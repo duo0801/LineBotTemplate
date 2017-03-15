22a744d290ec1c4ded8ab7eba2c17824d901ccd0
HsiaoPo hsuan <duo0801@gmail.com>
2017-03-15 15:53:01 +0800

    no message

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, event := range events {
-		if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(string(event))).Do(); err != nil {
+		if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(string(event.Type))).Do(); err != nil {
 					log.Print(err)
 				}
 		if event.Type == linebot.EventTypeMessage {
