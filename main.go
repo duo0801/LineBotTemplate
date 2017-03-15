1d21bfda3e632083ecca86ffed39ac6421faf798
HsiaoPo hsuan <duo0801@gmail.com>
2017-03-15 15:05:14 +0800

    no message

--- main.go
+++ main.go
@@ -59,10 +59,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
 					log.Print(err)
 				}
-			case *linebot.EventTypeJoin:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("歡迎加入欣峰")).Do(); err != nil {
-					log.Print(err)
-				}
+		}
+		else if event.Type == linebot.EventTypeJoin {
+			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("歡迎加入欣峰")).Do(); err != nil {
+				log.Print(err)
 			}
 		}
 	}
