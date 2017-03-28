6b1012916b0938f88e360cdd52f75ca1ba5271ed
HsiaoPo hsuan <duo0801@gmail.com>
2017-03-28 20:31:30 +0800

    no message

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				// message.ID+":"+message.Text+" (replaces)"
-				var msg = event.Source.UserID;
+				var msg = message.ID;
 
 				log.Println(message.Text)
 				if strings.Contains(message.Text, "常見問答") {
